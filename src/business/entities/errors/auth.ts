244304ca56649b47a5b084f821d8c8fbce2166ba
plozovik <plozovikov@gmail.com>
2025-04-29 18:45:28 +0200

    removed not used errors

--- src/business/entities/errors/auth.ts
+++ src/business/entities/errors/auth.ts
@@ -3,15 +3,3 @@ export class AuthenticationError extends Error {
     super(message, options);
   }
 }
-
-export class UnauthenticatedError extends Error {
-  constructor(message: string, options?: ErrorOptions) {
-    super(message, options);
-  }
-}
-
-export class UnauthorizedError extends Error {
-  constructor(message: string, options?: ErrorOptions) {
-    super(message, options);
-  }
-}
